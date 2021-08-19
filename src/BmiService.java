9f922417ef7991535887648887f413bdaf505bc6
Maria Salnikova <salnikovam1209@gmail.com>
2021-08-19 23:47:27 +0700

    change code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate (int bodyMass, double height) {
-        double bodyMassIndex = bodyMass / height * height;
+        double bodyMassIndex = bodyMass / (height * height);
         return bodyMassIndex;
     }
 }
